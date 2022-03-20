28ef6715308dab8817c16df6b4703e2bdbfae3aa
mahdiyarz <m.arbabzi@gmail.com>
2022-03-20 23:34:28 +0330

    Create dummy list view of movies

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -5,9 +5,38 @@ class Movie extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List movies = [
+      "Titanic",
+      "Age of twomorrow",
+      "12 Angry men",
+      "Room",
+      "Scar face",
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Movies"),
+        backgroundColor: Colors.teal,
+      ),
+      body: ListView.builder(
+        itemCount: movies.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            color: Colors.green,
+            child: ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: Colors.tealAccent,
+                child: Text(
+                  "M",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              trailing: const Icon(Icons.abc),
+              subtitle: const Text("more"),
+              title: Text(movies[index]),
+            ),
+          );
+        },
       ),
     );
   }
