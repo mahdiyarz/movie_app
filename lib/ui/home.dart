f60ce39a2e86c236f42ba57c4cee762204173ac5
mahdiyarz <m.arbabzi@gmail.com>
2022-03-21 00:57:20 +0330

    Create new rought for movie details

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -18,11 +18,11 @@ class Movie extends StatelessWidget {
         title: const Text("Movies"),
         backgroundColor: Colors.teal,
       ),
+      backgroundColor: Colors.teal[100],
       body: ListView.builder(
         itemCount: movies.length,
         itemBuilder: (BuildContext context, int index) {
           return Card(
-            color: Colors.green,
             child: ListTile(
               leading: const CircleAvatar(
                 backgroundColor: Colors.tealAccent,
@@ -34,10 +34,38 @@ class Movie extends StatelessWidget {
               trailing: const Icon(Icons.abc),
               subtitle: const Text("more"),
               title: Text(movies[index]),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return MovieDetails(movieName: movies[index]);
+                    },
+                  ),
+                );
+              },
             ),
           );
         },
       ),
+    );
+  }
+}
+
+// New Routgh
+class MovieDetails extends StatelessWidget {
+  final String? movieName;
+
+  const MovieDetails({Key? key, this.movieName}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(movieName!),
+        backgroundColor: Colors.teal,
+      ),
+      backgroundColor: Colors.teal[100],
     );
   }
 }
